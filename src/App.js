509fdb565778764260b3af844c8d6b7c7f2a8092
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-21 18:14:14 +0800

    2 ways of passing params to a function.

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ class App extends Component {
     ]
   };
 
-  updateNameHandler = () => {
+  updateNameHandler = (newName) => {
     this.setState({
       persons: [
-        { id: 1, name: "Lisa", origin: 33 },
+        { id: 1, name: newName, origin: 33 },
         { id: 2, name: "Jonathan", origin: 233 },
         { id: 3, name: "Koko Hekmatyar", origin: 112 }
       ]
@@ -26,7 +26,14 @@ class App extends Component {
       <div className="App">
         <h1>React 101</h1>
         <div>
-          <button onClick={this.updateNameHandler}>Click to Update!</button>
+          {/* Inefficient - This gets executed immediately when React component renders */}
+          {/* In fact, React wouldn't event render this */}
+          {/* <button onClick={this.updateNameHandler()}>Click to Update!</button> */}
+
+          <button onClick={() => this.updateNameHandler("Wiley")}>Click to Update!</button>
+
+          {/* This doesn't get executed immediately. This style of writing has implicit "return" due to "()" */}
+          {/* Alternatively, it to make it multiline, it can be written: onClick={() => {return this.updateNameHandler("Lisard!")} */}
           <Person
             name={this.state.persons[0].name}
             origin={this.state.persons[0].origin}
@@ -36,8 +43,9 @@ class App extends Component {
             <span>Is it limited to only 1? </span>
             <span>Apparently not. </span>
           </Person>
+          {/* This is the most preferred way of passing params. Use bind */}
           <Person
-            click={this.updateNameHandler}
+            click={this.updateNameHandler.bind(this, "Jonah")}
             name={this.state.persons[1].name}
             origin={this.state.persons[1].origin}
           />
