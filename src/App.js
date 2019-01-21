cf0d6d4f784c23ebea8e56beddcc6a2ad593635b
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-21 12:22:00 +0800

    Using props.children to render complex html.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,23 @@ class App extends Component {
       <div className="App">
         <h1>React 101</h1>
         <div>
-          <p>Hello, it's Monday</p>
-          <Person name="Lisa" origin={window.crypto.getRandomValues(new Uint32Array(1))[0]} />
-          <Person name="Jonah" origin={window.crypto.getRandomValues(new Uint32Array(1))[0]} />
-          <Person name="Koko" origin={window.crypto.getRandomValues(new Uint32Array(1))[0]} />  
+          <Person
+            name="Lisa"
+            origin={window.crypto.getRandomValues(new Uint32Array(1))[0]}
+          >
+            <span>I'm a customized content. </span>
+            <span>How many of me can be passed? </span>
+            <span>Is it limited to only 1? </span>
+            <span>Apparently not. </span>
+          </Person>
+          <Person
+            name="Jonah"
+            origin={window.crypto.getRandomValues(new Uint32Array(1))[0]}
+          />
+          <Person
+            name="Koko"
+            origin={window.crypto.getRandomValues(new Uint32Array(1))[0]}
+          />
         </div>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 // ES6 syntax. Read: a constant variable called 'person' equals to some function whereby the parts are: i) argument, ii) arrow function, iii) function body
-const person = (props) => {
-  return <p>I'm {props.name} from {props.origin}.</p>;
+const person = props => {
+  return (
+    <div>
+      <p>
+        I'm {props.name} from {props.origin}.
+      </p>
+      <div>{props.children}</div>
+    </div>
+  );
 };
 
 export default person;
-              
