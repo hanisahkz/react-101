b60cfc807d9ec614280824375eeddbb2da07e49e
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-21 09:22:43 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
+    /* jshint ignore:start */
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React 101</h1>
+        <div>
+          <p>Hello, it's Monday</p>
+          <Person />
+        </div>
       </div>
     );
+    /* jshint ignore:end */
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// ES6 syntax. Read: a constant variable called 'person' equals to some function whereby the parts are: i) argument, ii) arrow function, iii) function body
+const person = () => {
+  return <p>Hello from person file!</p>;
+};
+
+export default person;
